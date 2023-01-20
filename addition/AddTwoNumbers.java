77b45c178bc821df0b134392a813981c795870fe
Pallavi Pandit <ppandit@dxc.com>
2023-01-20 15:51:00 +0530

    First commit : adding two numbers in Java

---
+++ addition/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+public class AddTwoNumbers
+{
+	public static void main(String args[])
+	{
+		int firstno = 5;
+		int secondno = 6;
+		System.out.println("First no is"+firstno);
+		System.out.println("Second no is"+secondno);
+		int sum = firstno + secondno;
+		System.out.println("Sum is:"+sum);
+	}
+}
